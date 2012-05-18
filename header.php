20ce6bb18ef182abed0fc2c9ee9da284d2c623dc
Jason Lengstorf <jason.lengstorf@copterlabs.com>
2012-05-18 12:49:43 -0700

    Added "last modified" query string to stylesheet link and cleaned up other markup

--- header.php
+++ header.php
@@ -13,29 +13,19 @@
 <html <?php language_attributes(); ?>>
 <head>
 
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <title><?php
-    	/*
-    	 * Print the <title> tag based on what is being viewed.
-    	 * We filter the output of wp_title() a bit -- see
-    	 * rw_filter_wp_title() in functions.php.
-    	 */
-    	wp_title( '&rsaquo;', true, 'right' );
-    
-    	?></title>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <title><?php wp_title(); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
-    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); echo '?' . filemtime(bloginfo('stylesheet_url'); ?>" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
-	/* If not using Disqus, uncomment this code snippet
-	 * to support sites with threaded comments (when in use).
-	 */
-/*
-	if ( is_singular() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
-*/
+    // not using disqus? uncomment the following
+    /*
+    	if ( is_singular() && get_option( 'thread_comments' ) )
+    		wp_enqueue_script( 'comment-reply' );
+    */
 
-	wp_head();
+    wp_head();
 ?>
 </head>
 
@@ -43,18 +33,17 @@
 
     <header>
 
-    	<h1>
-    		<a href="<?php echo home_url( '/' ); ?>" 
-    		   title="<?php echo esc_attr(get_bloginfo('name', 'display')), ' &mdash; ', esc_attr(get_bloginfo( 'description' )); ?>" 
-    		   rel="home"><?php bloginfo( 'name' ); ?></a>
-    	</h1>
+        <h1>
+            <a href="<?php echo home_url( '/' ); ?>" 
+               title="<?php echo esc_attr(get_bloginfo('name', 'display')), ' &mdash; ', esc_attr(get_bloginfo( 'description' )); ?>" 
+               rel="home"><?php bloginfo( 'name' ); ?></a>
+        </h1>
 
-    	<nav id="access" role="navigation">
-            <a href="#content" class="skip-to-content" 
-               title="<?php esc_attr_e( 'Skip to content', 'rotorwash' ); ?>"><?php _e( 'Skip to content', 'rotorwash' ); ?></a>
-    		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
-    	</nav><!-- #access -->
+        <nav id="access" role="navigation">
+            <?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+        </nav>
 
     </header>
 
     <section id="content">
+
