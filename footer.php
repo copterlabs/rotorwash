f86b4cb968e03dfbb9084dd1121db1a73e7e3fd1
Jason Lengstorf <jason.lengstorf@copterlabs.com>
2012-05-18 18:08:24 -0700

    Updated FB like button to use FB page URL if it gets supplied

--- footer.php
+++ footer.php
@@ -6,6 +6,13 @@
  * @subpackage RotorWash
  * @since RotorWash 1.0
  */
+
+    $opts = get_option('rw_theme_settings');
+    if( !empty($opts['fb_page_id']) ) {
+    	$data_href = $opts['fb_page_id'];
+    } else {
+    	$data_href = get_site_url();
+    }
 ?>
 
     </section>
@@ -16,7 +23,7 @@
            title="<?php echo esc_attr(get_bloginfo('name', 'display')), ' &mdash; ', esc_attr(get_bloginfo( 'description' )); ?>" 
            rel="home"><?php bloginfo( 'name', 'display' ); ?></a>
         <a href="http://www.copterlabs.com/" title="Web design for the terminally rad">Web design by Copter Labs</a>
-        <div class="fb-like" data-href="<?php site_url(); ?>" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false"></div>
+        <div class="fb-like" data-href="<?=$data_href?>" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false"></div>
     </footer>
 
 <?php wp_footer(); ?>
