4dacea82ce9b66e5ac23ab3e0897dbda067cb6ff
Jason Lengstorf <jason.lengstorf@copterlabs.com>
2012-05-18 22:25:07 -0700

    Fixed indentation

--- assets/functions/filters.php
+++ assets/functions/filters.php
@@ -106,12 +106,12 @@ add_filter( 'wp_page_menu_args', 'rw_page_menu_args' );
  */
 function rw_trim_excerpt( $text='' )
 {
-	$text = strip_shortcodes( $text );
-	$text = apply_filters('the_content', $text);
-	$text = str_replace(']]>', ']]&gt;', $text);
-	$excerpt_length = apply_filters('excerpt_length', 55);
-	$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
-	return wp_trim_words( $text, $excerpt_length, $excerpt_more );
+    $text = strip_shortcodes( $text );
+    $text = apply_filters('the_content', $text);
+    $text = str_replace(']]>', ']]&gt;', $text);
+    $excerpt_length = apply_filters('excerpt_length', 55);
+    $excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
+    return wp_trim_words( $text, $excerpt_length, $excerpt_more );
 }
 add_filter('wp_trim_excerpt', 'rw_trim_excerpt');
 
