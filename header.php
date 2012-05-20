af01849c193c28c33ad858c196194fdc94ea0859
Jason Lengstorf <jason.lengstorf@copterlabs.com>
2012-05-19 17:07:17 -0700

    Fixed syntax error in style link

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
     <meta charset="<?php bloginfo('charset'); ?>" />
     <title><?php wp_title(); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
-    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); echo '?' . filemtime(bloginfo('stylesheet_url'); ?>" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); echo '?' . filemtime(bloginfo('stylesheet_url')); ?>" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
     // not using disqus? uncomment the following
