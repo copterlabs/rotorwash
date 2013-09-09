c1e2baf05e53aa2812ca917c037ac76b9718970b
jlengstorf <jason.lengstorf@copterlabs.com>
2013-09-08 22:42:23 -0700

    Removed unused functions, changed the way settings pages are handled, added a bunch of new settings, updated older functions, added WP media uploader integration to custom settings file inputs.

--- assets/functions/actions.php
+++ assets/functions/actions.php
@@ -12,6 +12,7 @@
  * 
  * @return  void
  * @since   1.0.2
+ * @deprecated 2.0.0
  */
 function rw_add_fb_root(  )
 {
@@ -31,13 +32,14 @@ function rw_add_fb_root(  )
 
 <?php
 }
-add_action('wp_footer', 'rw_add_fb_root');
+// add_action('wp_footer', 'rw_add_fb_root');
 
 /**
  * Adds Google +1 button to the footer of the site
  * 
  * @return  void
  * @since   1.0.5
+ * @deprecated 2.0.0
  */
 function rw_add_gplus_root(  )
 {
@@ -52,7 +54,7 @@ function rw_add_gplus_root(  )
 
 <?php
 }
-add_action('wp_footer', 'rw_add_gplus_root');
+// add_action('wp_footer', 'rw_add_gplus_root');
 
 /**
  * Adds Facebook Open Graph tags to the website using the wp_head action.
@@ -64,7 +66,7 @@ add_action('wp_footer', 'rw_add_gplus_root');
  * @since   1.0.2
  * @return  void
  */
-function rw_add_fb_og_tags()
+function rw_add_fb_og_tags(  )
 {
     // Bails if Yoast SEO is installed to avoid duplicate OG tags
     include_once ABSPATH . 'wp-admin/includes/plugin.php';
@@ -75,57 +77,58 @@ function rw_add_fb_og_tags()
         return FALSE;
     }
 
+    // Loads settings
     $opts = get_option('rw_theme_settings');
 
-    $locale     = strtolower(get_locale()); // This avoids a warning in the Facebook URL linter
-    $site_name  = get_bloginfo('name'); // Loads the name of the website
+    // Avoids a warning in the Facebook URL linter
+    $locale     = strtolower(get_locale());
+    $site_name  = get_bloginfo('name');
 
-    if( !empty($opts['fb_admins']) )
-    {
-        $fb_admins = '<meta property="fb:admins"      content="'.$opts['fb_admins'].'" />' . PHP_EOL;
-    }
-    else
-    {
+    // Adds Facebook admins if present
+    if (!empty($opts['fb_admins'])) {
+        $fb_admins = '<meta property="fb:admins"      '
+                   . 'content="'.$opts['fb_admins'].'" />' 
+                   . PHP_EOL;
+    } else {
         $fb_admins = NULL;
     }
 
-    if( !empty($opts['fb_appid']) )
-    {
-        $fb_appid = '<meta property="fb:app_id"      content="'.$opts['fb_appid'].'" />' . PHP_EOL;
-    }
-    else
-    {
+    // Adds the app ID if one is supplied
+    if (!empty($opts['fb_appid'])) {
+        $fb_appid = '<meta property="fb:app_id"      '
+                  . 'content="'.$opts['fb_appid'].'" />' 
+                  . PHP_EOL;
+    } else {
         $fb_appid = NULL;
     }
 
     // Checks for a default image set in the custom theme settings
     $def_img   = !empty($opts['default_image']) ? $opts['default_image'] : '';
 
-    if( is_single() )
-    {
+    if (is_single()) {
         global $post; // Brings the post into the function scope
-        if( get_the_post_thumbnail($post->ID, 'thumbnail') )
-        {
+        if (get_the_post_thumbnail($post->ID, 'thumbnail')) {
             $thumbnail_id = get_post_thumbnail_id($post->ID, 'thumbnail');
             $thumbnail_object = get_post($thumbnail_id);
             $image = $thumbnail_object->guid;
-        }
-        else
-        {
+        } else {
             $image = $def_img;
         }
 
-        $excerpt = !empty($post->post_excerpt) ? $post->post_excerpt : apply_filters('get_the_excerpt', $post->post_content);
+        // Generates an excerpt if one doesn't exist
+        if (!empty($post->post_excerpt)) {
+            $excerpt = $post->post_excerpt;
+        } else {
+            $excerpt = apply_filters('get_the_excerpt', $post->post_content);
+        }
 
         // Gets entry-specific info for display
         $title       = $post->post_title;
         $url         = get_permalink($post->ID);
         $type        = "article";
         $description = trim(strip_tags($excerpt));
-    }
-    else
-    {
-        // For non-blog posts (pages, home page, etc.), we display website info only
+    } else {
+        // Non-blog posts/pages (home page, loops, etc.) display site info only
         $title       = $site_name;
         $url         = site_url();
         $image       = $def_img;
@@ -156,24 +159,39 @@ add_action('wp_head', 'rw_add_fb_og_tags');
  * @return  void
  * @since   1.0
  */
-function rw_enqueue_scripts(  )
+function rw_enqueue_assets(  )
 {
-    wp_enqueue_script('jquery');
+    // Set the location of the assets folder
+    $assets_dir = get_template_directory_uri() . '/assets';
 
-    wp_enqueue_script(
-        'html5shiv',
-        '//html5shiv.googlecode.com/svn/trunk/html5.js'
+    // Registers styles
+    wp_enqueue_style(
+        'main',
+        $assets_dir . '/css/main.css',
+        FALSE,
+        filemtime(get_stylesheet_directory() . '/assets/css/main.css')
     );
 
-    wp_enqueue_script(
-        'twitter_widgets', 
-        '//platform.twitter.com/widgets.js', 
+    // Include a local copy of jQuery
+    wp_dequeue_script('jquery');
+    wp_deregister_script('jquery');
+
+    wp_register_script(
+        'jquery', 
+        $assets_dir . '/js/lib/jquery.js', 
         NULL, 
-        FALSE, 
+        '2.0.3', 
+        FALSE
+    );
+
+    wp_enqueue_script('main',
+        $assets_dir . '/js/main.min.js',
+        array('jquery'),
+        '1.0.0',
         TRUE
     );
 }
-add_action('wp_enqueue_scripts', 'rw_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'rw_enqueue_assets');
 
 /**
  * Outputs Google Analytics tracking code if an ID is supplied
@@ -185,18 +203,18 @@ function rw_add_google_analytics(  )
 {
     $opts = get_option('rw_theme_settings');
     if( isset($opts['ga_id']) && !empty($opts['ga_id']) ):
+        // Determines the domain name
+        $domain = preg_replace('/^www\./', '', $_SERVER['SERVER_NAME']);
 ?>
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', '<?php echo $opts['ga_id']; ?>']);
-  _gaq.push(['_trackPageview']);
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+  ga('create', '<?php echo $opts["ga_id"]; ?>', '<?php echo $domain; ?>');
+  ga('send', 'pageview');
 
 </script>
 <?php
@@ -212,12 +230,35 @@ add_action('wp_footer', 'rw_add_google_analytics');
  */
 function rw_create_menu_item(  )
 {
-    $page_title = 'Settings for ' . wp_get_theme();
-    $btn_text = wp_get_theme() . ' Settings';
-    $btn_icon = get_bloginfo('template_directory') . '/assets/images/copter-wp-admin-icon.png';
-    add_menu_page($page_title, $btn_text, 'administrator', __FILE__, 'rw_settings_page', $btn_icon);
+    add_options_page(
+        'Settings for ' . wp_get_theme(), 
+        wp_get_theme() . ' Settings', 
+        'manage_rotorwash', 
+        'rotorwash_general', 
+        'rw_settings_page'
+    );
+
+    add_options_page(
+        'Social Settings',
+        'Social',
+        'manage_rotorwash',
+        'rotorwash_social',
+        'rw_social_page'
+    );
+
+    add_options_page(
+        'Site Analytics Settings',
+        'Analytics',
+        'manage_rotorwash',
+        'rotorwash_analytics',
+        'rw_analytics_page'
+    );
+
+    register_setting('rw-theme-settings', 'rw_theme_settings', 'rw_options_validate');
 
     add_action( 'admin_init', 'register_custom_settings' );
+    add_action( 'admin_init', 'register_social_settings' );
+    add_action( 'admin_init', 'register_analytics_settings' );
 }
 add_action('admin_menu', 'rw_create_menu_item');
 
@@ -227,26 +268,109 @@ add_action('admin_menu', 'rw_create_menu_item');
  * @return  void
  * @since   1.0.2
  */
-function register_custom_settings() 
-{
-
-    register_setting('rw-theme-settings', 'rw_theme_settings');
-
+function register_custom_settings(  ) {
     // Theme Settings
-    add_settings_section('rw-theme-settings', 'Theme Settings', 'rw_theme_settings_text', 'rw-theme-settings');
-    add_settings_field( 'default_image', 'Default Image', 'rw_default_image', 'rw-theme-settings', 'rw-theme-settings', array('label_for'=>'default_image'));
+    add_settings_section(
+        'rw-theme-settings', 
+        'Theme Settings', 
+        'rw_theme_settings_text', 
+        'rotorwash_general'
+    );
 
-    // Google Analytics
-    add_settings_section('rw-analytics-settings', 'Site Stats Settings', 'rw_ga_settings_text', 'rw-theme-settings');
-    add_settings_field( 'ga_id', 'Google Analytics ID', 'rw_ga_id', 'rw-theme-settings', 'rw-analytics-settings', array('label_for'=>'ga_id'));
+    add_settings_field(
+        'default_image', 
+        'Default Image', 
+        'rw_default_image', 
+        'rotorwash_general', 
+        'rw-theme-settings', 
+        array('label_for'=>'default_image')
+    );
 
-    // Facebook Stuff
-    add_settings_section('rw-facebook-settings', 'Facebook Settings', 'rw_fb_settings_text', 'rw-theme-settings');
-    add_settings_field( 'fb_page_url', 'Facebook Page URL', 'rw_fb_page_url', 'rw-theme-settings', 'rw-facebook-settings', array('label_for'=>'fb_page_url'));
-    add_settings_field( 'fb_admins', 'Facebook Admin IDs (optional)', 'rw_fb_admins', 'rw-theme-settings', 'rw-facebook-settings', array('label_for'=>'fb_admins'));
-    add_settings_field( 'fb_appid', 'Facebook Page ID (optional)', 'rw_fb_appid', 'rw-theme-settings', 'rw-facebook-settings', array('label_for'=>'fb_appid'));
+    add_settings_field(
+        'has_products', 
+        'Add the Products custom post type', 
+        'rw_has_products', 
+        'rotorwash_general', 
+        'rw-theme-settings', 
+        array('label_for'=>'has_products')
+    );
+
+    add_settings_field(
+        'has_services', 
+        'Add the Services custom post type', 
+        'rw_has_services', 
+        'rotorwash_general', 
+        'rw-theme-settings', 
+        array('label_for'=>'has_services')
+    );
+
+    add_settings_field(
+        'has_testimonials', 
+        'Add the Testimonials custom post type', 
+        'rw_has_testimonials', 
+        'rotorwash_general', 
+        'rw-theme-settings', 
+        array('label_for'=>'has_testimonials')
+    );
 
     do_action( 'custom_settings_hook' );
+}
+
+function register_social_settings(  ) {
+    // Facebook Stuff
+    add_settings_section(
+        'rw-facebook-settings', 
+        'Facebook Settings', 
+        'rw_fb_settings_text', 
+        'rotorwash_social'
+    );
+
+    add_settings_field(
+        'fb_page_url', 
+        'Facebook Page URL', 
+        'rw_fb_page_url', 
+        'rotorwash_social', 
+        'rw-facebook-settings', 
+        array('label_for'=>'fb_page_url')
+    );
+    add_settings_field(
+        'fb_admins', 
+        'Facebook Admin IDs (optional)', 
+        'rw_fb_admins', 
+        'rotorwash_social', 
+        'rw-facebook-settings', 
+        array('label_for'=>'fb_admins')
+    );
+    add_settings_field(
+        'fb_appid', 
+        'Facebook Page ID (optional)', 
+        'rw_fb_appid', 
+        'rotorwash_social', 
+        'rw-facebook-settings', 
+        array('label_for'=>'fb_appid')
+    );
+
+    do_action( 'custom_settings_hook' );
+}
+
+function register_analytics_settings(  ) {
+    add_settings_section(
+        'rw-analytics-settings', 
+        'Google Analytics', 
+        'rw_ga_settings_text', 
+        'rotorwash_analytics'
+    );
+
+    add_settings_field(
+        'ga_id',
+        'Google Analytics Tracking ID', 
+        'rw_ga_id', 
+        'rotorwash_analytics', 
+        'rw-analytics-settings', 
+        array('label_for'=>'ga_id')
+    );
+
+    do_action('custom_settings_hook');
 }
 
 /**
@@ -272,7 +396,40 @@ function rw_theme_settings_text(  )
  */
 function rw_default_image(  )
 {
-    rw_text_field(__FUNCTION__);
+    rw_upload(__FUNCTION__);
+}
+
+/**
+ * Sets whether or not a products page is required
+ * 
+ * @return  void
+ * @since   1.0.2
+ */
+function rw_has_products(  )
+{
+    rw_radio(__FUNCTION__);
+}
+
+/**
+ * Sets whether or not a services page is required
+ * 
+ * @return  void
+ * @since   1.0.2
+ */
+function rw_has_services(  )
+{
+    rw_radio(__FUNCTION__);
+}
+
+/**
+ * Sets whether or not a testimonials page is required
+ * 
+ * @return  void
+ * @since   1.0.2
+ */
+function rw_has_testimonials(  )
+{
+    rw_radio(__FUNCTION__);
 }
 
 /**
@@ -413,6 +570,66 @@ function rw_textarea( $func )
 }
 
 
+/**
+ * Creates a checkbox for the Theme settings page
+ * 
+ * @return  void
+ * @since   1.0.1
+ */
+function rw_radio( $func )
+{
+    $field = str_replace('rw_', '', $func);
+    $opts  = get_option('rw_theme_settings');
+    $yes   = (isset($opts[$field]) && $opts[$field]==='yes') ? 'checked' : '';
+    $no    = $yes==='checked'  ? '' : 'checked';
+?>
+
+<label>
+    <input type="radio" 
+           id="<?php echo $field; ?>_on" 
+           name="rw_theme_settings[<?php echo $field; ?>]" 
+           value="yes" <?php echo $yes; ?> />
+    Yes
+</label>
+<label>
+    <input type="radio" 
+           id="<?php echo $field; ?>_off" 
+           name="rw_theme_settings[<?php echo $field; ?>]" 
+           value="no" <?php echo $no; ?> />
+    No
+</label>
+
+<?php
+}
+
+function rw_upload( $func ) {  
+    $field = str_replace('rw_', '', $func);
+    $opts = get_option('rw_theme_settings');
+    $value = isset($opts[$field]) ? $opts[$field] : '';
+    $preview = (!empty($value)) ? '<img src="' . $value . '" />' : NULL;
+?>  
+
+<input type="hidden" 
+       id="<?php echo $field; ?>" 
+       class="rotorwash-admin-upload-text"
+       name="rw_theme_settings[<?php echo $field; ?>]" 
+       value="<?php echo $value; ?>" />  
+<input id="<?php echo $field; ?>_button" 
+       type="button" 
+       class="button rotorwash-admin-upload" 
+       value="Upload" />
+<php if (!empty($value)): ?>
+<input id="<?php echo $field;?>_delete" 
+       type="submit" 
+       class="button" 
+       name="rw_theme_settings[<?php echo $field; ?>_delete]" 
+       value="Delete" />
+<span class="description">Choose an image from your hard drive or the media library.</span>
+<div class="rotorwash-admin-upload-preview"><?php echo $preview; ?></div>
+
+<?php  
+}  
+
 
 /**
  * Loads the custom theme settings area
@@ -422,6 +639,22 @@ function rw_textarea( $func )
  */
 function rw_settings_page(  )
 {
+    $page_title = "Settings for " . wp_get_theme();
+    $custom_settings = 'rotorwash_general';
+    require_once TEMPLATEPATH . '/assets/includes/rotorwash-settings.php';
+}
+
+function rw_social_page(  )
+{
+    $page_title = "Social Settings";
+    $custom_settings = 'rotorwash_social';
+    require_once TEMPLATEPATH . '/assets/includes/rotorwash-settings.php';
+}
+
+function rw_analytics_page(  )
+{
+    $page_title = "Analytics Settings";
+    $custom_settings = 'rotorwash_analytics';
     require_once TEMPLATEPATH . '/assets/includes/rotorwash-settings.php';
 }
 
@@ -429,8 +662,67 @@ function rw_settings_page(  )
 function rw_admin_enqueue_scripts(  )
 {
     wp_enqueue_style('rotorwash_admin_styles');
+
+    if (get_current_screen()->id==='settings_page_rotorwash_general') {
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('thickbox');
+        wp_enqueue_style('thickbox');
+        wp_enqueue_script('media-upload');
+        wp_enqueue_script('rw_admin_upload',
+            get_template_directory_uri() . '/assets/js/admin-upload.js',
+            array('jquery', 'media-upload', 'thickbox'),
+            '1.0.0'
+        );
+    }
 }
 add_action('admin_enqueue_scripts', 'rw_admin_enqueue_scripts');
+
+function replace_thickbox_text($translated_text, $text ) {  
+    if ('Insert into Post'===$text) {
+        $referer = strpos(wp_get_referer(), 'rotorwash_general');
+        if ($referer!=='') {
+            return "Use This Image";
+        }
+    }
+
+    return $translated_text;
+}
+
+function rw_options_validate( $input ) {
+    $opts = get_option('rw_theme_settings');
+    $opts = array_merge($opts, $input);
+    
+    $submit = !empty($input['submit']) ? TRUE : FALSE;
+    $delete_image = !empty($input['default_image_delete']) ? TRUE : FALSE;
+    
+    if ($submit && isset($input['default_image'])) {
+        if (
+            isset($opts['default_image'])
+            && $opts['default_image']!==$input['default_image'] 
+            && $opts['default_image']!==''
+        ) {
+            delete_image($opts['default_image']);
+        }
+    } else if ($delete_image) {
+        delete_image($opts['default_image']);
+        $opts['default_image'] = '';
+    }
+    
+    return $opts;
+}
+
+function delete_image( $image_url ) {
+    global $wpdb;
+    
+    // We need to get the image's meta ID..
+    $query = "SELECT ID FROM wp_posts where guid = '" . esc_url($image_url) . "' AND post_type = 'attachment'";  
+    $results = $wpdb -> get_results($query);
+
+    // And delete them (if more than one attachment is in the Library
+    foreach ($results as $row) {
+        wp_delete_attachment($row->ID);
+    }   
+}
 
 /**
  * Register custom styles for the admin dashboard widget
@@ -440,7 +732,12 @@ add_action('admin_enqueue_scripts', 'rw_admin_enqueue_scripts');
  */
 function rw_admin_init(  )
 {
-    wp_register_style('rotorwash_admin_styles', get_template_directory_uri() . '/assets/styles/admin.css');
+    wp_register_style('rotorwash_admin_styles', get_template_directory_uri() . '/assets/css/admin.css');
+
+    global $pagenow;
+    if ('media-upload.php'===$pagenow || 'async-upload.php'===$pagenow) {
+        add_filter('gettext', 'replace_thickbox_text', 1, 3);
+    }
 }
 add_action('admin_init', 'rw_admin_init');
 
@@ -499,3 +796,27 @@ function rw_add_dashboard_widget(  )
 </p>
 <?php
 }
+
+function rw_progress_alert(  ) {
+    if (!current_user_can('manage_rotorwash')) {
+        return FALSE;
+    }
+
+    $opts = get_option('rw_theme_settings');
+
+    if (!isset($opts['ga_id']) || empty($opts['ga_id'])):
+        $nag_message = '<strong>Important:</strong> In order to track visitors to your site, <a href="' 
+                     . get_bloginfo('wpurl') 
+                     . '/wp-admin/options-general.php?page=rotorwash_analytics">please add your Google Analytics ID here.</a>';
+?>
+<script type="text/javascript">
+jQuery(function($){
+    $('.wrap > h2')
+        .parent().prev()
+        .after('<div class="update-nag"><?php echo $nag_message; ?></div>');
+});
+</script>
+<?php
+    endif;
+}
+add_action('admin_head','rw_progress_alert');
